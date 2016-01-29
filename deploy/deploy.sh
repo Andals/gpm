7d74baf7b7b0a0ce08f3eca4e26a7c116b353cf8
ligang <ligang1109@aliyun.com>
2016-01-29 14:42:15 +0800

    add deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+curPath=`dirname $0`
+cd $curPath/../
+prjHome=`pwd`
+
+if [ $# -lt 1 ]
+then
+    echo "Usage $0 host1 host2 ..."
+    exit 1
+fi
+
+
+echo "Enter your password of online hosts: "
+read -s password
+sshCmd="sshpass -p $password ssh -o StrictHostKeyChecking=no"
+scpCmd="sshpass -p $password scp -o StrictHostKeyChecking=no"
+
+
+echo "Building binary"
+deployTmpDir=$prjHome/tmp/deploy
+if [ -d $deployTmpDir ]
+then
+    rm -rf $deployTmpDir
+fi
+mkdir -p $deployTmpDir
+
+binName=gpvm
+cd $deployTmpDir
+go build -o $binName $prjHome/main/main.go
+
+
+installDstDir=/usr/local/bin
+for host in $*
+do
+    echo Deploy to $host
+    $scpCmd $binName $host:./
+    $sshCmd -t $host "echo $password | sudo -S mv $binName $installDstDir"
+done
+
+
+rm -rf $deployTmpDir
